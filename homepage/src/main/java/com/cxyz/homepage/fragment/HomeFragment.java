16f1689c61a2a32b6e5bdd279125cc004f9c1ba3
q956822950 <39487187+q956822950@users.noreply.github.com>
2019-01-30 13:34:15 +0800

    1.30  13.33 暂时修改主页UI

--- homepage/src/main/java/com/cxyz/homepage/fragment/HomeFragment.java
+++ homepage/src/main/java/com/cxyz/homepage/fragment/HomeFragment.java
@@ -1,17 +1,21 @@
 package com.cxyz.homepage.fragment;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.alibaba.android.arouter.launcher.ARouter;
+import com.bumptech.glide.Glide;
 import com.cxyz.commons.fragment.BaseFragment;
 import com.cxyz.commons.utils.DateUtil;
 import com.cxyz.commons.utils.ToastUtil;
+import com.cxyz.commons.widget.TitleView;
 import com.cxyz.homepage.R;
 import com.cxyz.homepage.acitivity.CheckRedordActivity;
 import com.cxyz.homepage.acitivity.ClazzActivity;
@@ -23,6 +27,11 @@ import com.cxyz.homepage.ipresenter.impl.IHomePresenterImpl;
 import com.cxyz.homepage.iview.IHomeView;
 import com.cxyz.logiccommons.manager.UserManager;
 import com.cxyz.logiccommons.typevalue.PowerType;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.Transformer;
+import com.youth.banner.listener.OnBannerListener;
+import com.youth.banner.loader.ImageLoader;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -33,8 +42,11 @@ import java.util.Map;
  * Created by 鱼塘主 on 2018/9/25.
  */
 @Route(path="/homepage/HomeFragment")
-public class HomeFragment extends BaseFragment<IHomePresenter> implements IHomeView{
-
+public class HomeFragment extends BaseFragment<IHomePresenter> implements IHomeView,OnBannerListener {
+    private TitleView tv_homepage_title;
+    private Banner banner;
+    private ArrayList<String> list_path;
+    private ArrayList<String> list_title;
     //Adapter需要的数据
     ArrayList<Map<String,Object>> data;
 
@@ -45,7 +57,7 @@ public class HomeFragment extends BaseFragment<IHomePresenter> implements IHomeV
 
     @Override
     protected int getLayoutId() {
-        return R.layout.fragment_home_layout;
+        return R.layout.homepagelayout;
     }
 
     public static HomeFragment newInstance() {
@@ -56,9 +68,9 @@ public class HomeFragment extends BaseFragment<IHomePresenter> implements IHomeV
     @Override
     protected void initData(Bundle bundle) {
         String texts[] = new String[]{"考勤","考勤统计","班级课表","请假申请","上传假条","请假审核","数据导出","导入数据"};
-        int imgs[] = new int[]{R.mipmap.checkin, R.mipmap.checkcharts, R.mipmap.monthclass,
-                R.mipmap.appointapply, R.mipmap.classlists, R.mipmap.gradescase, R.mipmap.statistics,
-                R.mipmap.othercondition};
+        int imgs[] = new int[]{R.mipmap.appa, R.mipmap.appb, R.mipmap.appc,
+                R.mipmap.appd, R.mipmap.appe, R.mipmap.appf, R.mipmap.appg,
+                R.mipmap.apph};
         data = new ArrayList<>();
         Map<String, Object> map = null;
         switch (UserManager.getInstance().getUser().getPower())
@@ -106,6 +118,39 @@ public class HomeFragment extends BaseFragment<IHomePresenter> implements IHomeV
     protected void initView(View view, Bundle savedInstanceState) {
         gv_function = findViewById(R.id.gv_function);
         gv_function.setAdapter(new FunctionAdapter(getActivity(),data,R.layout.item_function_layout));
+        banner=findViewById(R.id.banner);
+        //放图片地址的集合
+        list_path = new ArrayList<>();
+        //放标题的集合
+        list_title = new ArrayList<>();
+        list_path.add("http://ww4.sinaimg.cn/large/006uZZy8jw1faic21363tj30ci08ct96.jpg");
+        list_path.add("http://ww4.sinaimg.cn/large/006uZZy8jw1faic259ohaj30ci08c74r.jpg");
+        list_path.add("http://ww4.sinaimg.cn/large/006uZZy8jw1faic2b16zuj30ci08cwf4.jpg");
+        list_path.add("http://ww4.sinaimg.cn/large/006uZZy8jw1faic2e7vsaj30ci08cglz.jpg");
+        list_title.add("好好学习");
+        list_title.add("天天向上");
+        list_title.add("热爱劳动");
+        list_title.add("不搞对象");
+        //设置内置样式，共有六种可以点入方法内逐一体验使用。
+        banner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR_TITLE_INSIDE);
+        //设置图片加载器，图片加载器在下方
+        banner.setImageLoader(new MyLoader());
+        //设置图片网址或地址的集合
+        banner.setImages(list_path);
+        //设置轮播的动画效果，内含多种特效，可点入方法内查找后内逐一体验
+        banner.setBannerAnimation(Transformer.Default);
+        //设置轮播图的标题集合
+        banner.setBannerTitles(list_title);
+        //设置轮播间隔时间
+        banner.setDelayTime(3000);
+        //设置是否为自动轮播，默认是“是”。
+        banner.isAutoPlay(true);
+        //设置指示器的位置，小点点，左中右。
+        banner.setIndicatorGravity(BannerConfig.CENTER)
+                //以上内容都可写成链式布局，这是轮播图的监听。比较重要。方法在下面。
+                .setOnBannerListener(this)
+                //必须最后调用的方法，启动轮播图。
+                .start();
     }
 
     @Override
@@ -211,4 +256,15 @@ public class HomeFragment extends BaseFragment<IHomePresenter> implements IHomeV
         builder.create().show();
     }
 
+    @Override
+    public void OnBannerClick(int position) {
+        ToastUtil.showLong("你点击了第"+position+"项");
+    }
+
+    private class MyLoader extends ImageLoader {
+        @Override
+        public void displayImage(Context context, Object path, ImageView imageView) {
+            Glide.with(context).load((String) path).into(imageView);
+        }
+    }
 }
