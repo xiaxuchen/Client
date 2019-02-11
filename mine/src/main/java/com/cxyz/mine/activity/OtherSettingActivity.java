c2bb89dbf0bf5d6d63e3c840467df2e7d6d25e13
q956822950 <39487187+q956822950@users.noreply.github.com>
2019-02-11 14:05:26 +0800

    2.11     14.02  暂时修改主页UI

---
+++ mine/src/main/java/com/cxyz/mine/activity/OtherSettingActivity.java
@@ -0,0 +1,138 @@
+package com.cxyz.mine.activity;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Switch;
+
+import com.alibaba.android.arouter.launcher.ARouter;
+import com.cxyz.commons.activity.BaseActivity;
+import com.cxyz.commons.manager.ActivityStackManager;
+import com.cxyz.commons.utils.AppUtil;
+import com.cxyz.commons.utils.ToastUtil;
+import com.cxyz.commons.widget.TitleView;
+import com.cxyz.logiccommons.manager.UserManager;
+import com.cxyz.logiccommons.service.UpdateService;
+import com.cxyz.mine.IPresenter.IMineFragmentPresenter;
+import com.cxyz.mine.IPresenter.impl.IMineFragmentPresenterlmpl;
+import com.cxyz.mine.R;
+import com.cxyz.mine.imodel.IMineFragmentModel;
+import com.cxyz.mine.iview.IMineFragementView;
+import com.qmuiteam.qmui.widget.roundwidget.QMUIRoundButton;
+
+import java.io.File;
+
+/**
+ * Created by Administrator on 2019/2/3.
+ */
+
+public class OtherSettingActivity extends BaseActivity<IMineFragmentPresenter> implements IMineFragementView{
+    private TitleView tv_othersetting_title;
+    private LinearLayout ll_othersetting_checkupdate;
+    private LinearLayout ll_othersetting_userresp;
+    private LinearLayout ll_othersetting_update;
+    private QMUIRoundButton bt_othersetting_exit;
+    private Switch sw_update;
+    private Dialog dialog;
+    private ProgressBar pb_pro;
+    @Override
+    public int getContentViewId() {
+        return R.layout.othersetting_layout;
+    }
+
+    @Override
+    public void initView() {
+        tv_othersetting_title = findViewById(R.id.tv_othersetting_title);
+        tv_othersetting_title.setTitle("更多设置");
+        ll_othersetting_checkupdate = findViewById(R.id.ll_othersetting_checkupdate);
+        ll_othersetting_userresp = findViewById(R.id.ll_othersetting_userresp);
+        ll_othersetting_update = findViewById(R.id.ll_othersetting_update);
+        bt_othersetting_exit = findViewById(R.id.bt_othersetting_exit);
+        sw_update = findViewById(R.id.sw_update);
+        sw_update.setChecked(getSpUtil().getBoolean("update", true));
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void setEvent() {
+        tv_othersetting_title.setBackClickListener(new TitleView.OnBackClickListener() {
+            @Override
+            public void onBackClick(View v) {
+                onBackPressed();
+            }
+        });
+        bt_othersetting_exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //跳转到考勤界面
+                ARouter.getInstance().build("/main/LoginActivity").navigation();
+                UserManager.getInstance().setUser(null);
+                getSpUtil().remove("pwd");
+                ActivityStackManager.getActivityStackManager().popAllActivity();
+            }
+        });
+        sw_update.setOnCheckedChangeListener((compoundButton, b) -> getSpUtil().putBoolean("update", b));
+        ll_othersetting_checkupdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                iPresenter.Update();
+            }
+        });
+    }
+
+
+    @Override
+    protected IMineFragmentPresenter createIPresenter() {
+        return new IMineFragmentPresenterlmpl();
+    }
+
+    @Override
+    public void showUpdateView(int versionCode, String des, final String url) {
+        AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
+        builder.setIcon(R.mipmap.common_logo);
+        builder.setTitle("发现新版本"+versionCode);
+        builder.setMessage(des);
+        builder.setPositiveButton("更新", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(getActivity(),UpdateService.class);
+                intent.putExtra("apkUrl",url);
+                getActivity().startService(intent);
+                dialog.dismiss();
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        dialog = builder.create();
+        dialog.show();
+    }
+
+    @Override
+    public void showDownload(int progress, int max) {
+        if(pb_pro != null)
+            pb_pro.setVisibility(View.VISIBLE);
+    }
+
+
+    @Override
+    public void installApp(File app) {
+        AppUtil.installApk(getActivity(),app.getAbsolutePath());
+    }
+
+    @Override
+    public void noUpdate() {
+        ToastUtil.showShort("已是最新版本");
+    }
+}
--- mine/src/main/java/com/cxyz/mine/fragment/MineFragment.java
+++ mine/src/main/java/com/cxyz/mine/fragment/MineFragment.java
@@ -1,16 +1,17 @@
 package com.cxyz.mine.fragment;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
-import android.widget.TextView;
-
+import android.widget.RelativeLayout;
 import com.alibaba.android.arouter.facade.annotation.Route;
-import com.alibaba.android.arouter.launcher.ARouter;
 import com.cxyz.commons.fragment.BaseFragment;
 import com.cxyz.commons.utils.AppUtil;
 import com.cxyz.commons.utils.ToastUtil;
@@ -19,11 +20,11 @@ import com.cxyz.mine.IPresenter.IMineFragmentPresenter;
 import com.cxyz.mine.IPresenter.impl.IMineFragmentPresenterlmpl;
 import com.cxyz.mine.R;
 import com.cxyz.mine.activity.ChangePwdACtivity;
-import com.cxyz.mine.activity.MoreSettingActivity;
 import com.cxyz.mine.activity.MyinfoActivity;
+import com.cxyz.mine.activity.OtherSettingActivity;
 import com.cxyz.mine.activity.UserResponse;
 import com.cxyz.mine.iview.IMineFragementView;
-
+import com.qmuiteam.qmui.widget.QMUIRadiusImageView;
 import java.io.File;
 
 /**
@@ -31,21 +32,22 @@ import java.io.File;
  */
 @Route(path = "/mine/MineFragment")
 public class MineFragment extends BaseFragment<IMineFragmentPresenter> implements View.OnClickListener ,IMineFragementView {
+    private ImageView iv_mine_othersetting;
+    private LinearLayout ll_mine_pc;
+    private LinearLayout ll_mine_update;
+    private LinearLayout ll_mine_resetpwd;
+    private LinearLayout ll_mine_about;
+    private QMUIRadiusImageView iv_mine_pic;
+    private RelativeLayout ll_mine_info;
     private Dialog dialog;
     private ProgressBar pb_pro;
-    private TextView tv_mine_myinfo;
-    private TextView tv_mine_update;
-    private TextView tv_mine_useradvice;
-    private TextView tv_mine_setting;
-    private TextView tv_mine_vac;
-    private  TextView   tv_mine_alterpwd;
     public static MineFragment newInstance() {
         return new MineFragment();
     }
 
     @Override
     protected int getLayoutId() {
-        return R.layout.fragment_mine_layout;
+        return R.layout.minelayoutmore;
     }
 
     @Override
@@ -54,12 +56,15 @@ public class MineFragment extends BaseFragment<IMineFragmentPresenter> implement
 
     @Override
     protected void initView(View view, Bundle savedInstanceState) {
-        tv_mine_myinfo = findViewById(R.id.tv_mine_myinfo);
-        tv_mine_vac = findViewById(R.id.tv_mine_vac);
-        tv_mine_update = findViewById(R.id.tv_mine_update);
-        tv_mine_useradvice = findViewById(R.id.tv_mine_useradvice);
-        tv_mine_setting = findViewById(R.id.tv_mine_setting);
-        tv_mine_alterpwd = findViewById(R.id.tv_mine_alterpwd);
+        iv_mine_othersetting=findViewById(R.id.iv_mine_othersetting);
+        ll_mine_pc=findViewById(R.id.ll_mine_pc);
+        ll_mine_update=findViewById(R.id.ll_mine_update);
+        ll_mine_resetpwd=findViewById(R.id.ll_mine_resetpwd);
+        ll_mine_about=findViewById(R.id.ll_mine_about);
+        iv_mine_pic=findViewById(R.id.iv_mine_pic);
+        ll_mine_info=findViewById(R.id.ll_mine_info);
+        iv_mine_pic.setImageResource(R.mipmap.belief);
+        iv_mine_pic.setCircle(true);
     }
 
     @Override
@@ -69,33 +74,30 @@ public class MineFragment extends BaseFragment<IMineFragmentPresenter> implement
 
     @Override
     protected void setListener() {
-        tv_mine_myinfo.setOnClickListener(this);
-        tv_mine_vac.setOnClickListener(this);
-        tv_mine_update.setOnClickListener(this);
-        tv_mine_useradvice.setOnClickListener(this);
-        tv_mine_setting.setOnClickListener(this);
-        tv_mine_alterpwd.setOnClickListener(this);
+        ll_mine_info.setOnClickListener(this);
+        ll_mine_update.setOnClickListener(this);
+        iv_mine_othersetting.setOnClickListener(this);
     }
 
     //监听事件
 
     //界面跳转
     //从我的界面跳转到设置界面
-    public void tv_mine_setting() {
-        Intent intent = new Intent(getActivity().getApplicationContext(), MoreSettingActivity.class);
+    public void iv_mine_othersetting() {
+        Intent intent = new Intent(getActivity().getApplicationContext(), OtherSettingActivity.class);
         startActivity(intent);
         mActivity.overridePendingTransition(R.anim.enter_next, R.anim.enter_exit);
 
     }
 
     //从我的界面跳转到个人信息界面
-    public void tv_mine_myinfo() {
+    public void ll_mine_info() {
         Intent intent = new Intent(getActivity().getApplicationContext(), MyinfoActivity.class);
         startActivity(intent);
         mActivity.overridePendingTransition(R.anim.enter_next, R.anim.enter_exit);
     }
     //从我的界面跳转到修改密码界面
-    public void tv_mine_alterpwd() {
+    public void ll_mine_resetpwd() {
         Intent intent = new Intent(getActivity().getApplicationContext(), ChangePwdACtivity.class);
         startActivity(intent);
         mActivity.overridePendingTransition(R.anim.enter_next, R.anim.enter_exit);
@@ -111,46 +113,40 @@ public class MineFragment extends BaseFragment<IMineFragmentPresenter> implement
     @Override
     public void onClick(View v) {
         int viewId = v.getId();
-        if(viewId == R.id.tv_mine_myinfo)
-            tv_mine_myinfo();
-        else if(viewId == R.id.tv_mine_vac)
-            ARouter.getInstance().build("/vac/MineVacateActivity").navigation();
-        else if(viewId == R.id.tv_mine_setting)
-            tv_mine_setting();
-        else if(viewId == R.id.tv_mine_alterpwd)
-            tv_mine_alterpwd();
-        else if(viewId == R.id.tv_mine_useradvice)
-            tv_mine_useradvice();
-        else if(viewId == R.id.tv_mine_update){
-            tv_mine_update();
+        if(viewId == R.id.ll_mine_info)
+            ll_mine_info();
+        else if(viewId == R.id.iv_mine_othersetting)
+            iv_mine_othersetting();
+        else  if (viewId==R.id.ll_mine_update){
+            ll_mine_update();
         }
     }
-    public void tv_mine_update(){
-     iPresenter.Update();
+    public void ll_mine_update(){
+        iPresenter.Update();
 
     }
 
     @Override
     public void showUpdateView(int versionCode, String des, final String url) {
-            AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
-            builder.setIcon(R.mipmap.common_logo);
-            builder.setTitle("发现新版本"+versionCode);
-            builder.setMessage(des);
-            builder.setPositiveButton("更新", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    Intent intent = new Intent(getActivity(),UpdateService.class);
-                    intent.putExtra("apkUrl",url);
-                    getActivity().startService(intent);
-                    dialog.dismiss();
-                }
-            });
-            builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which) {
-                    dialog.dismiss();
-                }
-            });
+        AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
+        builder.setIcon(R.mipmap.common_logo);
+        builder.setTitle("发现新版本"+versionCode);
+        builder.setMessage(des);
+        builder.setPositiveButton("更新", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Intent intent = new Intent(getActivity(),UpdateService.class);
+                intent.putExtra("apkUrl",url);
+                getActivity().startService(intent);
+                dialog.dismiss();
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
         dialog = builder.create();
         dialog.show();
     }
