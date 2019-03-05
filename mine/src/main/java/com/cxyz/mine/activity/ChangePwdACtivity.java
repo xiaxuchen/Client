fa6409cba6ed856c1c5257219d61ce2fe661d9b6
q956822950 <39487187+q956822950@users.noreply.github.com>
2019-03-05 20:53:34 +0800

    3.5     20.53  暂时修改主页底部UI，我的界面，完善修改密码UI

--- mine/src/main/java/com/cxyz/mine/activity/ChangePwdACtivity.java
+++ mine/src/main/java/com/cxyz/mine/activity/ChangePwdACtivity.java
@@ -1,5 +1,6 @@
 package com.cxyz.mine.activity;
 
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -13,6 +14,8 @@ import com.cxyz.mine.IPresenter.IChangePwdPresenter;
 import com.cxyz.mine.IPresenter.impl.IChangePwdPresenterImpl;
 import com.cxyz.mine.R;
 import com.cxyz.mine.iview.IChangePwdView;
+import com.maksim88.passwordedittext.PasswordEditText;
+import com.qmuiteam.qmui.widget.roundwidget.QMUIRoundButton;
 
 
 /**
@@ -20,8 +23,10 @@ import com.cxyz.mine.iview.IChangePwdView;
  */
 
 public class ChangePwdACtivity extends BaseActivity <IChangePwdPresenter>implements IChangePwdView{
-
-
+    private TitleView tv_changepwd_title;
+    private PasswordEditText et_changepwd_pwd;
+    private PasswordEditText et_changepwd_checkpwd;
+    private QMUIRoundButton bt_changepwd_finish;
     @Override
     public int getContentViewId() {
         return R.layout.activity_changepwds_layout;
@@ -29,7 +34,10 @@ public class ChangePwdACtivity extends BaseActivity <IChangePwdPresenter>impleme
 
     @Override
     public void initView() {
-
+        tv_changepwd_title=findViewById(R.id.tv_changepwd_title);
+        et_changepwd_pwd=findViewById(R.id.et_changepwd_pwd);
+        et_changepwd_checkpwd=findViewById(R.id.et_changepwd_checkpwd);
+        bt_changepwd_finish=findViewById(R.id.bt_changepwd_finish);
     }
 
     @Override
@@ -39,6 +47,20 @@ public class ChangePwdACtivity extends BaseActivity <IChangePwdPresenter>impleme
 
     @Override
     public void setEvent() {
+        tv_changepwd_title.setBackClickListener(new TitleView.OnBackClickListener() {
+            @Override
+            public void onBackClick(View v) {
+                onBackPressed();
+                getActivity().overridePendingTransition(R.anim.enter_next, R.anim.enter_exit);//动画过渡效果
+
+            }
+        });
+        bt_changepwd_finish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                checkpwd();
+            }
+        });
 
     }
 
@@ -55,5 +77,22 @@ public class ChangePwdACtivity extends BaseActivity <IChangePwdPresenter>impleme
     @Override
     public void chanegFail(String message) {
 
+    }
+    public void  checkpwd(){
+        String pwd=et_changepwd_pwd.getText().toString();
+        String checkpwd=et_changepwd_checkpwd.getText().toString();
+        if (pwd.equals(checkpwd)) {
+            if (!Validator.isPassword(pwd) && pwd.length() < 6) {
+                ToastUtil.showShort("您输入的密码过于简单，请输入正确格式！");
+            } else if (!Validator.isPassword(pwd) && pwd.length() > 20) {
+                ToastUtil.showShort("您输入的密码过于复杂，请输入正确格式！");
+            }
+        }
+        else ToastUtil.showShort("两次输入的密码不一致哦");
+    }
+    public void onBackPressed() {
+        //TODO something
+        super.onBackPressed();
+        getActivity().overridePendingTransition(R.anim.enter_next, R.anim.enter_exit);//动画过渡效果
     }
 }
--- mine/src/main/java/com/cxyz/mine/activity/ConfirmChangeActivity.java
+++ mine/src/main/java/com/cxyz/mine/activity/ConfirmChangeActivity.java
@@ -1,10 +1,16 @@
 package com.cxyz.mine.activity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.os.CountDownTimer;
+import android.support.annotation.ColorRes;
+import android.view.Display;
 import android.view.View;
 
 import com.cxyz.commons.IPresenter.IBasePresenter;
 import com.cxyz.commons.activity.BaseActivity;
+import com.cxyz.commons.widget.TitleView;
 import com.cxyz.mine.R;
 import com.qmuiteam.qmui.widget.roundwidget.QMUIRoundButton;
 
@@ -15,6 +21,7 @@ import com.qmuiteam.qmui.widget.roundwidget.QMUIRoundButton;
 public class ConfirmChangeActivity extends BaseActivity {
     private QMUIRoundButton bt_confirm_getcode;
     private QMUIRoundButton bt_confirm_next;
+    private TitleView tv_confirm_title;
     @Override
     public int getContentViewId() {
         return R.layout.activity_confirmchange_layout;
@@ -22,6 +29,7 @@ public class ConfirmChangeActivity extends BaseActivity {
 
     @Override
     public void initView() {
+        tv_confirm_title=findViewById(R.id.tv_confirm_title);
         bt_confirm_getcode=findViewById(R.id.bt_confirm_getcode);
         bt_confirm_next=findViewById(R.id.bt_confirm_next);
     }
@@ -33,11 +41,26 @@ public class ConfirmChangeActivity extends BaseActivity {
 
     @Override
     public void setEvent() {
+        tv_confirm_title.setBackClickListener(new TitleView.OnBackClickListener() {
+            @Override
+            public void onBackClick(View v) {
+                onBackPressed();
+                getActivity().overridePendingTransition(R.anim.enter_next, R.anim.enter_exit);//动画过渡效果
+            }
+        });
+        final MyCountDownTimer myCountDownTimer = new MyCountDownTimer(10000,1000);
         bt_confirm_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent=new Intent(getApplicationContext(),ChangePwdACtivity.class);
                 startActivity(intent);
+                getActivity().overridePendingTransition(R.anim.enter_next, R.anim.enter_exit);//动画过渡效果
+            }
+        });
+        bt_confirm_getcode.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myCountDownTimer.start();
             }
         });
     }
@@ -46,4 +69,48 @@ public class ConfirmChangeActivity extends BaseActivity {
     protected IBasePresenter createIPresenter() {
         return null;
     }
+    //倒计时函数
+    private class MyCountDownTimer extends CountDownTimer {
+
+        public MyCountDownTimer(long millisInFuture, long countDownInterval) {
+            super(millisInFuture, countDownInterval);
+        }
+
+        //计时过程
+        @Override
+        public void onTick(long l) {
+            //防止计时过程中重复点击
+            bt_confirm_getcode.setClickable(false);
+            bt_confirm_getcode.getBackground().getCurrent().setColorFilter(Color.parseColor("#cdcdcd"),PorterDuff.Mode.SRC_ATOP);
+            bt_confirm_getcode.setText(l/1000+"秒");
+
+        }
+
+        //计时完毕的方法
+        @Override
+        public void onFinish() {
+            //重新给Button设置文字
+            bt_confirm_getcode.setText("重新获取");
+            //设置可点击
+            bt_confirm_getcode.getBackground().getCurrent().setColorFilter(Color.parseColor("#0aa0ff"),PorterDuff.Mode.SRC_ATOP);
+            bt_confirm_getcode.setClickable(true);
+        }
+    }
+/*    private void clearTimer() {
+        if (task != null) {
+            task.cancel();
+            task = null;
+        }
+        if (timer != null) {
+            timer.cancel();
+            timer = null;
+        }
+    }*/
+@Override
+public void onBackPressed() {
+    //TODO something
+    super.onBackPressed();
+    getActivity().overridePendingTransition(R.anim.enter_next, R.anim.enter_exit);//动画过渡效果
+}
+
 }
